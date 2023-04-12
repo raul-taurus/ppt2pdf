6a9e3f1a43f3919f272e7f86d41cdaa6b8ee580b
Raul Li <raul.guoping@gmail.com>
2023-04-13 00:46:38 +0200

    first commit

---
+++ PowerPoint.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Office.Core;
+using Microsoft.Office.Interop.PowerPoint;
+using System.Runtime.InteropServices;
+
+class PowerPoint : IDisposable
+{
+    ApplicationClass _app;
+    Presentations _presentations;
+
+    public PowerPoint()
+    {
+        Console.WriteLine("Start PowerPoint Application...");
+        _app = new ApplicationClass();
+        _presentations = _app.Presentations;
+    }
+
+    public void ConvertToPdf(string pptFile)
+    {
+        Console.WriteLine("Open PPT file...");
+        var ppt = _presentations.Open(pptFile,
+            ReadOnly: MsoTriState.msoTrue,
+            Untitled: MsoTriState.msoFalse,
+            WithWindow: MsoTriState.msoTrue);
+
+        Console.WriteLine("Export as PDF...");
+        string pdfFile = Path.ChangeExtension(pptFile, ".pdf");
+        ppt.ExportAsFixedFormat2(pdfFile,
+            PpFixedFormatType.ppFixedFormatTypePDF,
+            Intent: PpFixedFormatIntent.ppFixedFormatIntentScreen,
+            FrameSlides: MsoTriState.msoFalse,
+            HandoutOrder: PpPrintHandoutOrder.ppPrintHandoutVerticalFirst,
+            OutputType: PpPrintOutputType.ppPrintOutputSlides,
+            PrintHiddenSlides: MsoTriState.msoFalse,
+            PrintRange: null,
+            RangeType: PpPrintRangeType.ppPrintAll,
+            SlideShowName: "",
+            IncludeDocProperties: true,
+            KeepIRMSettings: true,
+            DocStructureTags: false,
+            BitmapMissingFonts: true,
+            UseISO19005_1: true,
+            IncludeMarkup: true
+        );
+
+        ppt.Close();
+
+#pragma warning disable CA1416 // Validate platform compatibility
+        Marshal.ReleaseComObject(ppt);
+#pragma warning restore CA1416 // Validate platform compatibility
+    }
+
+    public void Dispose()
+    {
+#pragma warning disable CA1416 // Validate platform compatibility
+        Marshal.ReleaseComObject(_presentations);
+#pragma warning restore CA1416 // Validate platform compatibility
+        Console.WriteLine("Quit PowerPoint Application...");
+        _app.Quit();
+#pragma warning disable CA1416 // Validate platform compatibility
+        Marshal.ReleaseComObject(_app);
+#pragma warning restore CA1416 // Validate platform compatibility
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Office.Core;
+using Microsoft.Office.Interop.PowerPoint;
+using System.Runtime.InteropServices;
+
+if (args.Length > 0)
+{
+    if (Directory.Exists(args[0]))
+    {
+        using var app = new PowerPoint();
+        foreach (var pptFile in Directory.EnumerateFiles(args[0], "*", SearchOption.AllDirectories))
+        {
+            var ext = Path.GetExtension(pptFile).ToUpperInvariant();
+            if (ext == ".PPTX" || ext == ".PPT")
+            {
+                Console.WriteLine($"ConvertToPdf: {pptFile}");
+                app.ConvertToPdf(pptFile);
+            }
+        }
+    }
+    else
+    {
+        if (File.Exists(args[0]))
+        {
+            string pptFile = args[0];
+            using var app = new PowerPoint();
+            app.ConvertToPdf(pptFile);
+        }
+        else
+        {
+            Console.Error.WriteLine($"Cannot find file: {args[0]}");
+            return;
+        }
+    }
+}
+else
+{
+    Console.WriteLine($"Usae: exec <ppt filename>");
+    return;
+}
+
+Console.WriteLine("Bye!");
